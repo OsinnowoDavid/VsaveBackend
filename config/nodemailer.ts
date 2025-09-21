9c2638af8f958a36a93d2afcf1db301065d264d4
Danilo99 <omotehinse99@gmail.com>
2025-09-21 22:40:13 +0100

    deloyment process

--- config/nodemailer.ts
+++ config/nodemailer.ts
@@ -5,14 +5,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    host: "smtp.gmail.com", // Replace with the correct SMTP host
-    port: 465, // Correct port for secure SMTP
-    secure: true,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+  host: "box.eedu.tech",
+  port: 465,
+  secure: true, // true for 465, false for other ports
+  auth: {
+    user: process.env.User,
+    pass: process.env.Pass,
+  },
 });
 
 export default transporter;
