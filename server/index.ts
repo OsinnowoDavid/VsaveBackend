78362eba88a2f2cf2841ee2ee847b5d961b0b259
David-Netzence <david.osinnowo@netzence.com.ng>
2025-12-30 10:08:27 +0100

    cors

--- server/index.ts
+++ server/index.ts
@@ -23,6 +23,17 @@ app.use(
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE"],
         // allowedHeaders: ["Content-Type","Authorization"]
+         allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "X-Requested-With",
+    "Accept",
+    "Origin",
+    "Access-Control-Allow-Headers",
+    "Access-Control-Request-Headers"
+  ],
+  exposedHeaders: ["Set-Cookie"],
+  optionsSuccessStatus: 200
     }),
 );
 
