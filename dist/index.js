f81e17a4553d85c5816e041647fb6a29b8f0dfdc
danyboy99 <omotehinse99@gmail.com>
2026-01-05 09:47:00 +0100

    deactivate cors

--- dist/index.js
+++ dist/index.js
@@ -23,7 +23,7 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 // const allowedOrigin =[]
 app.use((0, cors_1.default)({
-    origin: "http://localhost:5173",
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
--- server/index.ts
+++ server/index.ts
@@ -19,7 +19,7 @@ app.use(express.json());
 // const allowedOrigin =[]
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
